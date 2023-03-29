81ce2123eb4dafa4dceea8a0186bddd58f230351
Georgios Kapak <geokapak@hotmail.com>
2023-03-29 14:05:51 +0200

    Initial upload

---
+++ report_1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 16 20:44:45 2022
+
+@author: msipek3
+"""
+import numpy as np
+import pandas as pd
+# Load the Iris csv data using the Pandas library
+
+# Import data
+data =  pd.read_csv('glass.csv', sep=",")
+
+# DATA CLEANING
+data.head()
+
+
+# DATA CLEANING
+
+
+
+print(data)
